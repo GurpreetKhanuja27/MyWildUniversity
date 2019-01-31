4770809e45c46c866c835070e60e7ba9688bd7b2
Gurpreet <gurpreet.khanuja01@gmail.com>
2019-01-31 13:35:19 -0500

    changed the uri

--- src/main/java/com/servlet/UniversityController.java
+++ src/main/java/com/servlet/UniversityController.java
@@ -18,7 +18,7 @@ import com.bean.Student;
 /**
  * Servlet implementation class UniversityController
  */
-@WebServlet("/controller")
+@WebServlet("/frontcontroller")
 public class UniversityController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
