fc66ab0dd3eb73b7694b75e6793d763980bf6199
Gurpreet <gurpreet.khanuja01@gmail.com>
2019-01-31 14:54:20 -0500

    changed jsp name

--- src/main/java/com/servlet/UniversityController.java
+++ src/main/java/com/servlet/UniversityController.java
@@ -32,7 +32,7 @@ public class UniversityController extends HttpServlet {
     	
     	if(action.equals("register")) {
     		
-    		view = request.getRequestDispatcher("registration.jsp");
+    		view = request.getRequestDispatcher("registerStudent.jsp");
     		view.forward(request, response);
     		
     	}else if(action.equals("list")) {    		 	
